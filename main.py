d820ac5ba56f616ce5e9d78cc6eebf29051bdf5e
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2023-10-15 22:52:28 +0300

    Implement Apartment Parsing via Telegram Bot

--- main.py
+++ main.py
@@ -3,142 +3,96 @@ import time
 import json
 from bs4 import BeautifulSoup
 
-# Токен вашого бота в Telegram
-TOKEN = "6505643635:AAGiVH7PloEUkrEYZTnygcBnNTQjhmDNzNM"
-# Загальний URL для роботи з Telegram API
-API_URL = f"https://api.telegram.org/bot{TOKEN}/"
-# Список підписників, які взаємодіють з ботом
-subscribers = []
+TELEGRAM_BOT_TOKEN = "6505643635:AAGiVH7PloEUkrEYZTnygcBnNTQjhmDNzNM"
+TELEGRAM_API_URL = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/"
+subscribed_users = []
 
 
-def get_olx_apartment_details():
-    """Отримує деталі квартир з OLX."""
-    url = "https://www.olx.ua/uk/nedvizhimost/kvartiry/dolgosrochnaya-arenda-kvartir/kiev/?currency=UAH&search%5Bfilter_float_price:from%5D=6000&search%5Bfilter_float_price:to%5D=9000"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
+def extract_apartment_information_from_olx():
+    olx_url = "https://www.olx.ua/uk/nedvizhimost/kvartiry/dolgosrochnaya-arenda-kvartir/kiev/?currency=UAH&search%5Bfilter_float_price:from%5D=6000&search%5Bfilter_float_price:to%5D=9000"
+    website_response = requests.get(olx_url)
 
-    # Ваші CSS селектори для отримання даних з OLX
-    apartments = soup.select(".some-class-for-apartments")
-    details_list = []
+    if website_response.status_code != 200:
+        print(f"Failed to fetch data from OLX. Status code: {website_response.status_code}")
+        return []
 
-    for apt in apartments:
-        image = apt.select_one(".some-class-for-image")["src"]
-        location = apt.select_one(".some-class-for-location").text.strip()
-        price = apt.select_one(".some-class-for-price").text.strip()
+    html_content = BeautifulSoup(website_response.content, 'html.parser')
 
-        details_list.append((image, location, price))
+    apartment_elements = html_content.select(".some-class-for-apartments")
+    apartment_information = []
 
-    return details_list
+    for apartment in apartment_elements:
+        image_url = apartment.select_one(".some-class-for-image")["src"]
+        apartment_location = apartment.select_one(".some-class-for-location").text.strip()
+        apartment_price = apartment.select_one(".some-class-for-price").text.strip()
 
+        apartment_information.append((image_url, apartment_location, apartment_price))
 
-def parse_dom_ria():
-    """Отримує деталі квартир з dom.ria."""
-    url = "https://dom.ria.com/uk/arenda-kvartir/kiev-nedorogo/"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-
-    # Ваші CSS селектори для отримання даних з dom.ria
-    apartments = soup.select(".specific-class-for-apartments")
-    details_list = []
-
-    for apt in apartments:
-        image = apt.select_one(".specific-class-for-image")["src"]
-        location = apt.select_one(".specific-class-for-location").text.strip()
-        price = apt.select_one(".specific-class-for-price").text.strip()
-
-        details_list.append((image, location, price))
-
-    return details_list
+    return apartment_information
 
 
-def parse_100realty():
-    """Отримує деталі квартир з 100realty."""
-    url = "https://100realty.ua/uk/realty_search/apartment/rent/p_4000_8000/cur_3/kch_2"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-
-    # Ваші CSS селектори для отримання даних з 100realty
-    apartments = soup.select(".another-specific-class-for-apartments")
-    details_list = []
-
-    for apt in apartments:
-        image = apt.select_one(".another-specific-class-for-image")["src"]
-        location = apt.select_one(".another-specific-class-for-location").text.strip()
-        price = apt.select_one(".another-specific-class-for-price").text.strip()
-
-        details_list.append((image, location, price))
-
-    return details_list
-
-
-def send_to_telegram(details_list):
-    """Надсилає деталі квартир у Telegram."""
-    for detail in details_list:
-        for subscriber in subscribers:
-            caption = f"Місце розташування: {detail[1]}\nЦіна: {detail[2]}"
-            data = {
-                "chat_id": subscriber,
-                "photo": detail[0],
-                "caption": caption
+def send_apartment_information_to_user(details_list):
+    for apartment_detail in details_list:
+        for user in subscribed_users:
+            information_caption = f"Location: {apartment_detail[1]}\nPrice: {apartment_detail[2]}"
+            data_to_send = {
+                "chat_id": user,
+                "photo": apartment_detail[0],
+                "caption": information_caption
             }
-            requests.post(API_URL + "sendPhoto", data=data)
+            requests.post(TELEGRAM_API_URL + "sendPhoto", data=data_to_send)
 
 
-def send_keyboard(chat_id, message):
-    """Надсилає клавіатуру (випадаючий список команд) у Telegram."""
-    keyboard = {
-        "keyboard": [[{"text": "/data"}, {"text": "/help"}]],
+def display_commands_to_user(chat_id, message_text):
+    command_keyboard = {
+        "keyboard": [[{"text": "/data"}, {"text": "/help"}, {"text": "/home"}]],
         "resize_keyboard": True
     }
-    payload = {
+    message_payload = {
         "chat_id": chat_id,
-        "text": message,
-        "reply_markup": json.dumps(keyboard)
+        "text": message_text,
+        "reply_markup": json.dumps(command_keyboard)
     }
-    requests.post(API_URL + "sendMessage", data=payload)
+    requests.post(TELEGRAM_API_URL + "sendMessage", data=message_payload)
 
 
-def check_updates(last_update_id=0):
-    """Перевіряє нові повідомлення в Telegram та реагує на команди."""
-    response = requests.get(API_URL + "getUpdates", params={"offset": last_update_id + 1})
-    updates = response.json().get("result", [])
+def check_new_telegram_messages(last_update_id=0):
+    telegram_response = requests.get(TELEGRAM_API_URL + "getUpdates", params={"offset": last_update_id + 1})
+    message_updates = telegram_response.json().get("result", [])
 
-    for update in updates:
-        chat_id = update["message"]["chat"]["id"]
-        message_text = update["message"]["text"]
+    for update in message_updates:
+        user_chat_id = update["message"]["chat"]["id"]
+        user_message_text = update["message"]["text"]
 
-        if message_text == "/data":
-            current_date = time.strftime("%Y-%m-%d")
-            requests.get(API_URL + "sendMessage", params={"chat_id": chat_id, "text": current_date})
+        if user_message_text == "/data":
+            current_date_string = time.strftime("%Y-%m-%d")
+            requests.get(TELEGRAM_API_URL + "sendMessage",
+                         params={"chat_id": user_chat_id, "text": current_date_string})
             continue
-        elif message_text == "/help":
-            help_message = "Виберіть команду з наведеного списку."
-            send_keyboard(chat_id, help_message)
+        elif user_message_text == "/help":
+            display_commands_to_user(user_chat_id, "Please select a command from the list.")
             continue
-        elif message_text.startswith("/"):
-            send_keyboard(chat_id, "Виберіть команду з наведеного списку.")
+        elif user_message_text == "/home":
+            olx_apartment_details = extract_apartment_information_from_olx()
+            if olx_apartment_details:
+                send_apartment_information_to_user([olx_apartment_details[0]])
+            continue
+        elif user_message_text.startswith("/"):
+            display_commands_to_user(user_chat_id, "Please select a command from the list.")
             continue
 
-        if chat_id not in subscribers:
-            subscribers.append(chat_id)
-            send_keyboard(chat_id, "Ви підписалися на оновлення! Виберіть команду з наведеного списку.")
+        if user_chat_id not in subscribed_users:
+            subscribed_users.append(user_chat_id)
+            display_commands_to_user(user_chat_id,
+                                     "You have subscribed for updates! Please select a command from the list.")
 
-    if updates:
-        return updates[-1]["update_id"]
+    if message_updates:
+        return message_updates[-1]["update_id"]
     return last_update_id
 
 
 if __name__ == "__main__":
-    last_update_id = 0
+    last_processed_update_id = 0
 
     while True:
-        last_update_id = check_updates(last_update_id)
-
-        details = get_olx_apartment_details()
-        send_to_telegram(details)
-
-        details = parse_dom_ria()
-        send_to_telegram(details)
-
-        details = parse_100realty()
-        send_to_telegram(details)
+        last_processed_update_id = check_new_telegram_messages(last_processed_update_id)
